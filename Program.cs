6ce3879ba519c4e5e73a9d9d4a9e82086778843c
Yeasa Mondal <yeasa1223@gmail.com>
2025-01-28 22:57:20 +0530

    controller and model added

--- Program.cs
+++ Program.cs
@@ -29,43 +29,7 @@ app.MapGet("/",()=>{
 
 List<Department> departments = new List<Department>();
 
-// read a departmennt GET: /api/departments
-app.MapGet("/api/departments",()=>{
-    return Results.Ok(departments);
-});
-
-// create a departmennt POST: /api/departments
-app.MapPost("/api/departments",([FromBody] Department departmentData)=>{
-    var newDepartment= new Department{
-        departmentid= Guid.NewGuid(),
-        departmentName= departmentData.departmentName,
-        managerId=departmentData.managerId
-    };
-    departments.Add(newDepartment);
-    return Results.Created($"/api/departments/{newDepartment.departmentid}",departments);
-    
-});
-
-// delete a departmennt GET: /api/departments
-app.MapDelete("/api/departments{departmentid}",(Guid depadepartmentid)=>{
-    var foundDepartment = departments.FirstOrDefault(Department => Department.departmentid == depadepartmentid);
-    if (foundDepartment==null){
-        return Results.NotFound($"department not found with {depadepartmentid} department id");
-    };
-    departments.Remove(foundDepartment);
-    return Results.NoContent();
-});
-
-app.MapPut("/api/departments",()=>{
-    var foundDepartment = departments.FirstOrDefault(Department => Department.departmentid==Guid.Parse("123e4567-e89b-12d3-a456-426614174000"));
-    if (foundDepartment==null){
-        return Results.NotFound($"department not found with {Guid.Parse("123e4567-e89b-12d3-a456-426614174000")} department id");
-    };
-    foundDepartment.departmentName="salls";
-    return Results.NoContent();
-});
-
-
+app.MapControllers();   
 app.Run();
 
 // DTO
